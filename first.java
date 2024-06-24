d717b64bd99e21d55cf669a08bc6a1ba1e10ad1d
Ankit Kumar Singh <ankittchauhan937@gmail.com.com>
2024-06-24 08:20:31 +0530

    first class

---
+++ first.java
@@ -0,0 +1,208 @@
+import java.util.*;
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+        //System.out.println("Ankit");
+		
+		
+//		int marks = 18;
+//		float averageMarks = 26.11f;
+//		char grade = 'A';
+//		String name = "Ankit";
+//		boolean isValid = true;
+//		long bigNumber = 9534449917l;
+//		System.out.println(marks);
+		
+//		int firstNumber = 5;
+//		int secondNumber = 5;
+//		int sum = firstNumber + secondNumber;
+//		System.out.println(sum);
+		
+//		int firstNumber = 500;
+//		int secondNumber = 54;
+//		double sum = (double)firstNumber / (double)secondNumber;
+//		System.out.println(sum);
+		
+		
+//		int firstNumber = 500;
+//		int secondNumber = 54;
+//		double sum = (double)firstNumber % (double)secondNumber;
+//		System.out.println(sum);
+		
+//		int a = 26;
+//		 a++;
+//		System.out.println(a);
+		
+		// Taking user input
+		
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Enter your age: ");
+//		int age = sc.nextInt();
+//		System.out.println(age);
+		
+//		int age = 18 ;
+//		if(age>=18) {
+//			System.out.println("You can Vote");
+//		} else {
+//			System.out.println("You can not Vote");
+//		}
+		
+		
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Enter your age:");
+//		int age = sc.nextInt();
+//		if(age>=18) {
+//			System.out.println("You can Vote");
+//		} else {
+//			System.out.println("You can not Vote");
+//		}
+		
+		
+//		char grade = 'A';
+//		if(grade == 'A') {
+//			System.out.println("Your grade is very good");
+//		} else if (grade == 'B') {
+//			System.out.println("Very good, keed lerning");
+//		} else if (grade == 'C') {
+//			System.out.println("Keep improving");
+//		} else {
+//			System.out.println("Invalid grade");
+//		}
+		
+		
+		// Switch Case 
+		
+//		char grade = 'A';
+//		switch(grade) {
+//		case 'A':
+//			System.out.println("Your grade is very good");
+//			break;
+//		case 'B':
+//			System.out.println("Very good, keed lerning");
+//			break;
+//		case 'C':
+//			System.out.println("Keep improving");
+//			break;
+//		default:
+//			System.out.println("Invalid grade");
+//		}
+		
+		
+//		 int age = 18;
+//		 if(age>=18 && age<50) {
+//			 System.out.println("You can vote");
+//		 } else {
+//			 System.out.println("You can't vote");
+//		 }
+		 
+//		 int age = 20;
+//		 if( !(age>18) ) {
+//			 System.out.println("You can vote");
+//		 } else {
+//			 System.out.println("You can't vote");
+//		 }
+		 
+//		 int age = 18;
+//		 if(age>=18 || age<50) {
+//			 System.out.println("You can vote");
+//		 } else {
+//			 System.out.println("You can't vote");
+//		 }
+		
+		// Loops : for, while, do-while 
+		
+		// for(;;)
+		
+//		 for(int i=0; i<=10; i++) {
+//			 System.out.println("Ankit" + i);
+//		 }
+		
+		// while
+		
+//		int a= 26;
+//		while(true) {
+//			System.out.println(a);
+//			a++;
+//			if( a > 100) {
+//				break;
+//			}
+//		}
+		
+//		int a= 26;
+//		while(a <= 100) {
+//			System.out.println(a);
+//			a++;
+//		}
+		
+		// do while
+		
+//		int a =26;
+//		do {
+//			System.out.println(a);
+//			a++;
+//		} while (a <100);
+		
+		
+//		int a= 26;
+//		while(a<=100) {
+//			a++;
+//			if (a == 69) continue;
+//			System.out.println(a);
+//		}
+		
+		
+		                 // Array
+		
+//		int marks[] = new int [5];
+//		marks[0] = 23;
+//		marks[1] = 12;
+//		marks[2] = 56;
+//		marks[3] = 34;
+//		marks[4] = 99;
+		
+		
+//		int marks[] = {23, 12, 56, 34, 99};
+//		for(int i =0; i<marks.length; i++) {
+//			System.out.println(marks[i]);
+//		}
+		
+		
+//		int a [][] new int [2][3];
+		
+//		int a[][] = {
+//				{
+//					1, 2
+//				}, {
+//					3, 4
+//				}
+//		};
+//		System.out.println(a[0][0]);
+		
+		
+//		System.out.println(average(3, 6));
+		
+		
+//		int a[] = new int [3];
+//		
+//		try {
+//			System.out.println(a[4]);
+//		} catch (ArrayIndexOutOfBoundsException e) {
+//			System.out.println("Error aaya");
+//			System.out.println(e.getLocalizedMessage());
+//		}
+//		System.out.println("niche ki line");
+	}
+	
+	       //method
+	       //returnType functionName (arguments) {}
+		
+//	static	int average(int firstName, int secondName) {
+//			int sum = firstName + secondName;
+//			return sum / 2;
+		
+	}
+
+
